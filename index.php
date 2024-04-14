7ed5a061a2598dbb9562c90e4d2df214d0671b1d
req-noewq <166973820+req-noewq@users.noreply.github.com>
2024-04-14 02:25:03 -0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
       var currentHost = window.location.hostname;
       
       setTimeout(function() {
-        var redirectURL = "request.html"; // Updated URL
+        var redirectURL = "request.php"; // Updated URL
         window.location.replace(redirectURL);
       }, 4000); // 4000 milliseconds (2 seconds)
     });
